d7e0fbfff87c792b51a052037d03903092b21492
Lionel Kaneza <lionkaneza@gmail.com>
2023-09-22 15:38:04 +0200

    refactor: set maximum width on navigation section

--- 02 - airbnb-clone/src/components/Navbar.jsx
+++ 02 - airbnb-clone/src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import logo from "../images/airbnb.png";
 export default function Navbar() {
   return (
     <nav className="py-5 shadow-md px-12">
-      <img src={logo} className="block"/>
+      <div className="max-w-xl mx-auto">
+        <img src={logo} className="block" />
+      </div>
     </nav>
   );
 }
