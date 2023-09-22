40e078d7b1c822c047938eef52a4a24ef1f7507f
Lionel Kaneza <lionkaneza@gmail.com>
2023-09-22 15:52:18 +0200

    refactor: hero and navbar responsive - 2

--- 02 - airbnb-clone/src/components/Hero.jsx
+++ 02 - airbnb-clone/src/components/Hero.jsx
@@ -6,10 +6,10 @@ export default function Hero() {
       <div className="max-w-xl mx-auto">
         <img src={heroImage} className="mx-auto" />
 
-        <h3 className="text-3xl self-start font-semibold mt-10">
+        <h3 className="text-xl sm:text-3xl self-start font-semibold mt-10">
           Online Experiences
         </h3>
-        <p className="text-xl mt-5">
+        <p className="text-base sm:text-xl mt-5">
           Join unique interactive activities led by one-of-a-kind hosts—all
           without leaving home.
         </p>
