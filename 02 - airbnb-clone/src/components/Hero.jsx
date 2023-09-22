c07d9c954164f66daee138a3ceec45006c3e0ab0
Lionel Kaneza <lionkaneza@gmail.com>
2023-09-22 15:50:55 +0200

    refactor: hero and navbar responsive

--- 02 - airbnb-clone/src/components/Hero.jsx
+++ 02 - airbnb-clone/src/components/Hero.jsx
@@ -2,9 +2,9 @@ import heroImage from "../images/grid.png";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center mt-10 px-12">
+    <div className="flex flex-col items-center mt-10 px-5 sm:px-12">
       <div className="max-w-xl mx-auto">
-        <img src={heroImage} />
+        <img src={heroImage} className="mx-auto" />
 
         <h3 className="text-3xl self-start font-semibold mt-10">
           Online Experiences
--- 02 - airbnb-clone/src/components/Navbar.jsx
+++ 02 - airbnb-clone/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import logo from "../images/airbnb.png";
 
 export default function Navbar() {
   return (
-    <nav className="py-5 shadow-md px-12">
+    <nav className="py-5 shadow-md px-5 sm:px-12">
       <div className="max-w-xl mx-auto">
         <img src={logo} className="block" />
       </div>
