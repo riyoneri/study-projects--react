b37a267bf49d8681e4bea00bdf5555655b3834a2
Lionel Kaneza <lionkaneza@gmail.com>
2023-09-25 18:07:36 +0200

    feat: form component [meme-generator]

--- 03 - meme-generator/src/App.js
+++ 03 - meme-generator/src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar";
+import Form from "./components/Form";
 
 export default function App() {
   return (
     <main>
       <Navbar />
-      <div>
-        
+      <div className="max-w-2xl mx-auto mt-10">
+        <Form />
       </div>
     </main>
   );
---
+++ 03 - meme-generator/src/components/Form.jsx
@@ -0,0 +1,22 @@
+export default function Form() {
+  return (
+    <form action="" method="" onSubmit={(e) => e.preventDefault()} className="flex flex-col gap-4">
+      <div className="flex justify-stretch gap-2">
+        <input
+          type="text"
+          className="border-neutral-300 rounded-md border flex-1 p-2 focus:outline-none"
+        />
+        <input
+          type="text"
+          className="border-neutral-300 rounded-md border flex-1 p-2 focus:outline-none"
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-gradient-to-r text-white from-[#672280] to-[#A626D3] rounded-md py-3"
+      >
+        Get a new meme image 🖼
+      </button>
+    </form>
+  );
+}
