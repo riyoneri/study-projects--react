3edf1b59254665445d5ee99377829b56a7c01cba
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-01 18:18:29 +0200

    refactor: put add keys to list items[coder-files-6]

--- 05 - coder-files/src/components/List.jsx
+++ 05 - coder-files/src/components/List.jsx
@@ -1,8 +1,10 @@
 export default function List({ array }) {
   return (
     <ul className="text-center">
-      {array.map((item) => (
-        <li className="py-3 border px-10 hover:bg-neutral-100">{item}</li>
+      {array.map((item, index) => (
+        <li key={index} className="py-3 border px-10 hover:bg-neutral-100">
+          {item}
+        </li>
       ))}
     </ul>
   );
