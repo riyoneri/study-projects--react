5857a1d9be6364238752090b11dd896dc234f3cd
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-01 13:38:24 +0200

    refactor: add ! on hello world[coder-files-1]

--- 05 - coder-files/src/App.jsx
+++ 05 - coder-files/src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-[#FEC800]">
-      <span className="text-2xl">Hello world</span>
+      <span className="text-2xl">Hello world!</span>
     </div>
   );
 }
