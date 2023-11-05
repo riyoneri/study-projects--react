5ac4fa36411dcff337da3c9746fb48b7af7bba72
Lionel Kaneza <lionkaneza@gmail.com>
2023-11-05 13:30:21 +0200

    feat: display todos accordingly[todo]

--- 10 - todo-app/src/App.jsx
+++ 10 - todo-app/src/App.jsx
@@ -5,9 +5,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
+import Todo from "./components/Todo";
+
 export default function App() {
   const [inputValue, setInputValue] = useState("");
   const [formError, setFormError] = useState(false);
+  const [todos, setTodos] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,6 +21,17 @@ export default function App() {
       setFormError(true);
       return;
     }
+
+    setTodos((prevTodos) => {
+      prevTodos.push({
+        checked: false,
+        text: formValue,
+        id: prevTodos.length,
+      });
+      return prevTodos;
+    });
+
+    setInputValue("");
   };
 
   const handleInputChange = (e) => {
@@ -25,8 +39,41 @@ export default function App() {
     setInputValue(e.target.value);
   };
 
+  const toggleComplete = (id) => {
+    const currentTodos = [...todos];
+
+    const targetTodo = currentTodos.find((todo) => todo.id === id);
+
+    targetTodo.checked = !targetTodo.checked;
+
+    currentTodos.splice(id, 1, targetTodo);
+
+    setTodos(currentTodos);
+  };
+
+  let pendingTodos = <div className="text-center">All tasks done</div>;
+  let completedTodos = <div className="text-center">No tasks completed</div>;
+
+  if (todos.some((todo) => !todo.checked)) {
+    pendingTodos = todos
+      .filter((todo) => !todo.checked)
+      .reverse()
+      .map((todo) => (
+        <Todo key={todo.id} {...todo} onToggleComplete={toggleComplete} />
+      ));
+  }
+
+  if (todos.some((todo) => todo.checked)) {
+    completedTodos = todos
+      .filter((todo) => todo.checked)
+      .reverse()
+      .map((todo) => (
+        <Todo key={todo.id} {...todo} onToggleComplete={toggleComplete} />
+      ));
+  }
+
   return (
-    <div className="bg-[#7AB8E1] min-h-screen flex text-sm sm:text-base px-5 sm:px-10 items-center justify-center">
+    <div className="bg-[#7AB8E1] py-10 min-h-screen flex text-sm sm:text-base px-5 sm:px-10 items-center justify-center">
       <div className="py-10 px-5 sm:px-10 bg-[#312F58] w-full md:w-1/2 xl:w-1/3 rounded-lg flex flex-col">
         <form onSubmit={handleSubmit}>
           <label
@@ -40,6 +87,7 @@ export default function App() {
               name="text"
               className="flex-1 bg-transparent focus:outline-none"
               onChange={handleInputChange}
+              value={inputValue}
             />
             <button className="text-xl sm:text-3xl font-bold bg-[#3276A6] p-1 sm:p-2 flex items-center justify-center rounded-full">
               <FontAwesomeIcon
@@ -51,32 +99,12 @@ export default function App() {
         </form>
         <div className="mt-10 text-white">
           <h3>TO DO</h3>
-          <div className="mt-3">
-            <div className="flex items-center gap-2 bg-[#414166] p-3 rounded-md">
-              <span className="h-5 w-5 bg-white rounded-full grid place-content-center cursor-pointer">
-                <FontAwesomeIcon icon={faCheck} className="h-4 text-black" />
-              </span>
-              <p className="flex-1 line-through">
-                This is my todoThis is my todo
-              </p>
-              <FontAwesomeIcon
-                icon={faTrash}
-                className="h-4 cursor-pointer hover:text-red-400 transition"
-              />
-            </div>
-          </div>
+          <div className="mt-3 space-y-2">{pendingTodos}</div>
         </div>
         <div className="mt-10 text-white">
           <h3>COMPLETED</h3>
-          <div className="mt-3">
-            <div className="flex items-center gap-2 bg-[#414166] p-3 rounded-md">
-              <span className="h-5 w-5 bg-white rounded-full grid place-content-center cursor-pointer">
-                <FontAwesomeIcon icon={faCheck} className="h-4 text-black" />
-              </span>
-              <p className="flex-1 line-through">
-                This is my todoThis is my todo
-              </p>
-            </div>
+          <div className="mt-3 space-y-2">
+            {completedTodos}
           </div>
         </div>
       </div>
