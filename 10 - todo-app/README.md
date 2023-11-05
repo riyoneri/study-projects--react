1f0f334508eb469a419e9e0dd01e994415b4a65a
Lionel Kaneza <lionkaneza@gmail.com>
2023-11-05 10:36:25 +0200

    feat: todo initial interface[to-do]

---
+++ 10 - todo-app/README.md
@@ -0,0 +1,1 @@
+This project is about Todo app 
---
+++ 10 - todo-app/src/App.jsx
@@ -0,0 +1,61 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faCheck, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+export default function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="bg-[#7AB8E1] min-h-screen flex px-5 sm:px-10 items-center justify-center">
+      <div className="py-10 px-5 sm:px-10 bg-[#312F58] w-full md:w-1/2 lg:w-1/3 rounded-lg flex flex-col">
+        <form onSubmit={handleSubmit}>
+          <label className="bg-[#27264D] transition border-2 border-red-400 px-3 py-2 rounded-full text-white flex gap-2 items-center">
+            <input
+              type="text"
+              name="text"
+              className="flex-1 bg-transparent focus:outline-none"
+            />
+            <button className="text-xl sm:text-3xl font-bold bg-[#3276A6] p-2 flex items-center justify-center rounded-full">
+              <FontAwesomeIcon
+                icon={faPlus}
+                className="h-5 w-5 cursor-pointer"
+              />
+            </button>
+          </label>
+        </form>
+        <div className="mt-10 text-white">
+          <h3>TO DO</h3>
+          <div className="mt-3">
+            <div className="flex items-center gap-2 bg-[#414166] p-3 rounded-md">
+              <span className="h-5 w-5 bg-white rounded-full grid place-content-center cursor-pointer">
+                <FontAwesomeIcon icon={faCheck} className="h-4 text-black" />
+              </span>
+              <p className="flex-1 line-through">
+                This is my todoThis is my todo
+              </p>
+              <FontAwesomeIcon
+                icon={faTrash}
+                className="h-4 cursor-pointer hover:text-red-400 transition"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-10 text-white">
+          <h3>COMPLETED</h3>
+          <div className="mt-3">
+            <div className="flex items-center gap-2 bg-[#414166] p-3 rounded-md">
+              <span className="h-5 w-5 bg-white rounded-full grid place-content-center cursor-pointer">
+                <FontAwesomeIcon icon={faCheck} className="h-4 text-black" />
+              </span>
+              <p className="flex-1 line-through">
+                This is my todoThis is my todo
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
